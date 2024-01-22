8674f7d760f11e894838981c0abb5d322853a41a
will258012 <will258012@gmail.com>
2024-01-22 22:00:19 +0800

    Merge branch 'main' into 1.18

--- common/src/main/java/com/undefinedbhvr/seaborgium/compat/iris/mixin/iris/MixinPipelineManager.java
+++ common/src/main/java/com/undefinedbhvr/seaborgium/compat/iris/mixin/iris/MixinPipelineManager.java
@@ -11,8 +11,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = PipelineManager.class, remap = false)
 public class MixinPipelineManager {
-    @Inject(method = "preparePipeline", at = @At(value = "INVOKE", target = "Lnet/coderbot/iris/IrisLogging;info(Ljava/lang/String;[Ljava/lang/Object;)V"))
-    private void sb$reload_on_pipeline_change(DimensionId dim, CallbackInfoReturnable<WorldRenderingPipeline> cir) {
+    @Inject(method = "preparePipeline", at = @At(value = "INVOKE", target = "Lnet/coderbot/iris/IrisLogging;info(Ljava/lang/String;[Ljava/lang/Object;)V"), cancellable=false)
+    private void sb$reload_on_pipeline_change(CallbackInfoReturnable<WorldRenderingPipeline> cir) {
         Seaborgium.LOGGER.warn("Shader cache needs invalidated. Reason: Iris Created New Pipeline");
         Seaborgium.invalidate_shaders();
     }
