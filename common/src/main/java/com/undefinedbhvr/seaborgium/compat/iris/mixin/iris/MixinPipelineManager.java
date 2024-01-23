3f86ba777638d2a7ccae4eea6ac7076492675fcf
will258012 <will258012@gmail.com>
2024-01-23 14:25:13 +0800

    Delete unnecessary items

--- common/src/main/java/com/undefinedbhvr/seaborgium/compat/iris/mixin/iris/MixinPipelineManager.java
+++ common/src/main/java/com/undefinedbhvr/seaborgium/compat/iris/mixin/iris/MixinPipelineManager.java
@@ -3,7 +3,6 @@ package com.undefinedbhvr.seaborgium.compat.iris.mixin.iris;
 import com.undefinedbhvr.seaborgium.Seaborgium;
 import net.coderbot.iris.pipeline.PipelineManager;
 import net.coderbot.iris.pipeline.WorldRenderingPipeline;
-import net.coderbot.iris.shaderpack.DimensionId;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
